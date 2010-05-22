de961c1e5a53efd7d3430b45b8764ad526695202
kyvinh <kyvinh@3fae8a4e-a88b-2036-a294-7357eb425605>
2010-05-22 23:09:52 +0000

    popup finally shows information

--- RecentWidget/src/org/recentwidget/EventPopupActivity.java
+++ RecentWidget/src/org/recentwidget/EventPopupActivity.java
@@ -1,34 +1,79 @@
 package org.recentwidget;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Contacts.People;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class EventPopupActivity extends Activity {
+
+	private static final String TAG = "RW:EventPopupActivity";
 
 	// private static final double WIDTH_RATIO = 0.9;
 	// private static final int MAX_WIDTH = 640;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 
+		int buttonPressed = getIntent().getIntExtra(
+				RecentWidgetProvider.BUTTON_PRESSED, -1);
+		if (buttonPressed == -1) {
+			Log.w(TAG,
+					"Started EventPopupActivity without ButtonPressed extra!");
+			finish();
+		}
+
+		RecentEvent recentEvent = RecentWidgetProvider
+				.getRecentEvent(buttonPressed);
+		if (recentEvent == null) {
+			Log.w(TAG, "ButtonPressed extra correspond to no RecentEvent!");
+			finish();
+		}
+
 		requestWindowFeature(Window.FEATURE_LEFT_ICON);
-		requestWindowFeature(Window.FEATURE_RIGHT_ICON);
 
 		setContentView(R.layout.eventpopup);
 
-		// Different icons: sym_call_incoming ic_dialog_info
+		// Different possible icons: sym_call_incoming ic_dialog_info
 
 		getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
 				android.R.drawable.ic_dialog_info);
 
-		getWindow().setFeatureDrawableResource(Window.FEATURE_RIGHT_ICON,
-				android.R.drawable.ic_delete);
+		// Set the content
 
-		// Does not work. TODO: Remove title bar and create custom title bar.
-		View closeButton = findViewById(android.R.drawable.ic_delete);
+		TextView textView = (TextView) findViewById(R.id.popupText);
+		textView.setText(recentEvent.getPerson());
+
+		ImageButton actionButton = (ImageButton) findViewById(R.id.popupAction);
+		final Intent actionIntent = new Intent(Intent.ACTION_VIEW);
+
+		if (recentEvent.getPersonId() == null) {
+			// Bring up the dialer since no Contact registered
+			actionIntent.setData(Uri.parse("tel:" + recentEvent.getNumber()));
+		} else {
+			// Show the contact page
+			actionIntent.setData(ContentUris.withAppendedId(People.CONTENT_URI,
+					recentEvent.getPersonId()));
+		}
+
+		actionButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(actionIntent);
+				finish();
+			}
+		});
 	}
-
 }
