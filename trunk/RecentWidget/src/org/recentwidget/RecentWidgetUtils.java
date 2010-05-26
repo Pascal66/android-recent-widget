66139a7db7b74b5e70b22233ef52f2eb13f40b87
kyvinh <kyvinh@3fae8a4e-a88b-2036-a294-7357eb425605>
2010-05-26 19:54:53 +0000

    refactoring to avoid copy/paste

--- trunk/RecentWidget/src/org/recentwidget/RecentWidgetUtils.java
+++ trunk/RecentWidget/src/org/recentwidget/RecentWidgetUtils.java
@@ -6,5 +6,6 @@ public class RecentWidgetUtils {
 
 	public static final String ACTION_SHOW_POPUP = "org.recentwidget.SHOW_POPUP";
 	public static final String ACTION_UPDATE_TELEPHONY = "org.recentwidget.UPDATE_TELEPHONY";
+	public static final String ACTION_UPDATE_SMS = "org.recentwidget.UPDATE_SMS";
 
 }
---
+++ trunk/RecentWidget/src/org/recentwidget/dao/CallLogDao.java
@@ -0,0 +1,58 @@
+package org.recentwidget.dao;
+
+import org.recentwidget.EventListBuilder;
+import org.recentwidget.RecentWidgetUtils;
+
+import android.database.Cursor;
+import android.provider.CallLog.Calls;
+import android.util.Log;
+
+public class CallLogDao extends ContentResolverTemplate implements
+		EventObserver {
+
+	private static final String TAG = "RW:CallLogDao";
+
+	public CallLogDao() {
+		super();
+
+		contentUri = Calls.CONTENT_URI;
+		projection = new String[] { Calls.CACHED_NAME, Calls.NUMBER, Calls.NEW,
+				Calls.TYPE, Calls.DATE };
+		sortOrder = Calls.DEFAULT_SORT_ORDER;
+
+	}
+
+	protected void extractEvent(EventListBuilder builder, Cursor callsCursor) {
+
+		// Cached name, may not be the name as in the Contacts
+
+		String name = callsCursor.getString(callsCursor
+				.getColumnIndex(Calls.CACHED_NAME));
+
+		String number = callsCursor.getString(callsCursor
+				.getColumnIndex(Calls.NUMBER));
+
+		// The type of the call (incoming, outgoing or missed).
+
+		int type = callsCursor.getInt(callsCursor.getColumnIndex(Calls.TYPE));
+
+		// The date the call occured in ms since the epoch
+
+		long date = callsCursor.getLong(callsCursor.getColumnIndex(Calls.DATE));
+
+		/*
+		 * RecentEvent event = new RecentEvent(); event.setPerson(name);
+		 * event.setNumber(number); event.setType(type);
+		 */
+
+		Log.v(TAG, "Fetched telephony recent event");
+
+		builder.add(name, number, type, date);
+	}
+
+	@Override
+	public boolean supports(String intentAction) {
+		return RecentWidgetUtils.ACTION_UPDATE_TELEPHONY.equals(intentAction);
+	}
+
+}
