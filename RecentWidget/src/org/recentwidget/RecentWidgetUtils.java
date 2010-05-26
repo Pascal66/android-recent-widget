30f9d579a799d2c21fe1a01391361664bea4bdb7
kyvinh <kyvinh@3fae8a4e-a88b-2036-a294-7357eb425605>
2010-05-26 21:22:56 +0000

    fixed receiving sms

--- RecentWidget/src/org/recentwidget/RecentWidgetUtils.java
+++ RecentWidget/src/org/recentwidget/RecentWidgetUtils.java
@@ -8,4 +8,5 @@ public class RecentWidgetUtils {
 	public static final String ACTION_UPDATE_TELEPHONY = "org.recentwidget.UPDATE_TELEPHONY";
 	public static final String ACTION_UPDATE_SMS = "android.provider.Telephony.SMS_RECEIVED";
 
+	public static final String MESSAGE_SENT_ACTION = "com.android.mms.transaction.MESSAGE_SENT";
 }
